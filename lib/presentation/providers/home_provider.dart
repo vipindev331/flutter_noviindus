abdcd7fcddf41f1efdc821ade5ad17db2ed3f72e
VipindevP <vipindevp@am.amrita.edu>
2026-02-25 14:40:52 +0530

    changed categories list for home screen

--- lib/presentation/providers/home_provider.dart
+++ lib/presentation/providers/home_provider.dart
@@ -20,41 +20,19 @@ class HomeProvider extends BaseProvider {
 
   Future<void> init() async {
     setLoading();
-    await Future.wait([_fetchCategories(), _fetchFeeds()]);
-  }
-
-  Future<void> _fetchCategories() async {
-    try {
-      final response = await _apiService.get('category_list');
-      final data = response.data as Map<String, dynamic>;
-      if (data['status'] == true) {
-        final list = data['categories'] as List? ?? [];
-        _categories = list
-            .map((e) => CategoryModel.fromJson(e as Map<String, dynamic>))
-            .toList();
-        notifyListeners();
-      }
-    } catch (_) {}
-  }
-
-  Future<void> _fetchFeeds() async {
     try {
       final response = await _apiService.get('home');
       final data = response.data as Map<String, dynamic>;
       if (data['status'] == true) {
-        // Parse feeds from 'results' key
         final results = data['results'] as List? ?? [];
         _feeds = results
             .map((e) => FeedModel.fromJson(e as Map<String, dynamic>))
             .toList();
 
-        // Use category_dict from home response if category_list returned nothing
-        if (_categories.isEmpty) {
-          final categoryDict = data['category_dict'] as List? ?? [];
-          _categories = categoryDict
-              .map((e) => CategoryModel.fromJson(e as Map<String, dynamic>))
-              .toList();
-        }
+        final categoryDict = data['category_dict'] as List? ?? [];
+        _categories = categoryDict
+            .map((e) => CategoryModel.fromJson(e as Map<String, dynamic>))
+            .toList();
 
         _feeds.isEmpty ? setEmpty() : setIdle();
       } else {
